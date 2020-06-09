186380f1e65f15e4bd0798eeeef7b59f691191b6
Filippe Braga <filippeffx@hotmail.com>
2020-06-09 15:08:52 -0300

    React part ended, need some fix

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import axios from 'axios';
@@ -31,10 +31,29 @@ const CreatePoint = () => {
   const [ufs, setUfs] = useState<string[]>([]);
   const [cities, setCities] = useState<string[]>([]);
 
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    whatsapp: '',
+  });
+
   const [selectedUf, setSelectedUf] = useState('0');
   const [selectedCity, setSelectedCity] = useState('0');
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
-  //counter
+
+  const history = useHistory();
+  
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+
+      setInitialPosition([latitude, longitude]);
+      // console.log(position);
+    });
+  }, []);
 
   useEffect(() => {
     api.get('items').then(res => {
@@ -84,6 +103,50 @@ const CreatePoint = () => {
     ])
   }
 
+  function handleSelectItem(id: number) {
+    const alreadySelected = selectedItems.findIndex(item => item === id);
+
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter(item => item !== id);
+
+      setSelectedItems(filteredItems);
+    } else {
+      setSelectedItems([...selectedItems, id]);
+    }    
+  }
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const { name, email, whatsapp } = formData;
+    const uf = selectedUf;
+    const city = selectedCity;
+    const [latitude, longitude] = selectedPosition;
+    const items = selectedItems;
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      uf,
+      city, 
+      latitude, 
+      longitude, 
+      items
+    };
+
+    await api.post('points', data);
+
+    history.push('/');
+  }
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    //console.log(event.target.name, event.target.value);
+
+    setFormData({ ...formData, [name]: value })
+  }
+
   return (
     <div id="page-create-point">
       <header>
@@ -95,7 +158,7 @@ const CreatePoint = () => {
         </Link>
       </header>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br /> ponto de coleta</h1>
 
         <fieldset>
@@ -109,6 +172,7 @@ const CreatePoint = () => {
               type="text"  
               name="name"
               id="name"
+              onChange={handleInputChange}
             />
           </div>
 
@@ -119,6 +183,7 @@ const CreatePoint = () => {
                 type="email"  
                 name="email"
                 id="email"
+                onChange={handleInputChange}
               />
             </div>
 
@@ -128,6 +193,7 @@ const CreatePoint = () => {
                 type="text"  
                 name="whatsapp"
                 id="whatsapp"
+                onChange={handleInputChange} 
               />
             </div>
 
@@ -140,14 +206,13 @@ const CreatePoint = () => {
             <span>Selecione o endereço no mapa</span>
           </legend>
 
-          <Map center={[-23.4715331, -46.6608874]} zoom={15} onClick={handleMapClick}>
+          <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
             <TileLayer
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
 
             <Marker position={selectedPosition} /> 
-
           </Map>
 
           <div className="field-group">
@@ -190,7 +255,11 @@ const CreatePoint = () => {
 
           <ul className="items-grid">
             {items.map(item => (
-              <li key={item.id}>
+              <li 
+                key={item.id} 
+                onClick={() => handleSelectItem(item.id)}
+                className={selectedItems.includes(item.id) ? 'selected' : ''}
+              >
                 <img src={item.image_url} alt={item.title}/>
                 <span>{item.title}</span>
               </li>            
