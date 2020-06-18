58b1ff166f2b479b0d8fc1cdd5b782865c5c5bd7
Filippe Braga <filippeffx@hotmail.com>
2020-06-17 23:40:50 -0300

    Configuring Navigation

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Feather as Icon } from '@expo/vector-icons';
 import { View, ImageBackground, Text, Image, StyleSheet } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
+  const navigation = useNavigation();
+
+  function handleNavigateToPoints() {
+    navigation.navigate('Points');
+  }
+
   return (
     <ImageBackground 
       source={require('../../assets/home-background.png')} 
@@ -15,7 +24,16 @@ const Home = () => {
       </View>
 
       <View style={styles.footer}>
-        
+        <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#FFF" size={24} />
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>
+            Entrar
+          </Text>
+        </RectButton>
       </View>
     </ImageBackground>
   );
@@ -25,7 +43,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
-    backgroundColor: '#F0F0F5'
   },
 
   main: {
@@ -65,6 +82,10 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.1)',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+
+  footer: {
+
   },
 
   buttonText: {
